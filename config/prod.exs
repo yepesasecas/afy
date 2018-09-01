c6abcfd56cb384163b07747d4ac539a6c432461c
Andres Yepes <yepes07@gmail.com>
2018-08-31 22:34:02 -0500

    fix: remove assets manifest

--- config/prod.exs
+++ config/prod.exs
@@ -17,8 +17,7 @@ config :afy, AfyWeb.Endpoint,
   load_from_system_env: true,
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "example.com", port: 80]
 
 config :afy, Afy.Repo,
   adapter: Ecto.Adapters.Postgres,
