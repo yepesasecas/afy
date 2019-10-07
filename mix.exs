21e8549213d92ddee62f2a0aae6ca6a5d366b523
Andres Yepes <yepes07@gmail.com>
2019-10-06 20:42:41 -0500

    update deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Afy.Mixfile do
     [
       app: :afy,
       version: "0.0.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
