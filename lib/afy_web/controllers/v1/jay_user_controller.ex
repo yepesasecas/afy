3822fa4b123be1bd9366aa1c999e42e38f7a61e7
Andres Yepes <yepes07@gmail.com>
2019-10-13 21:11:49 -0500

    chore: update test DB Config username

--- lib/afy_web/controllers/v1/jay_user_controller.ex
+++ lib/afy_web/controllers/v1/jay_user_controller.ex
@@ -15,7 +15,7 @@ defmodule AfyWeb.V1.JayUserController do
     with {:ok, %User{} = user} <- Jay.create_user(user_params) do
       conn
       |> put_status(:created)
-      # |> put_resp_header("location", user_path(conn, :show, user))
+      |> put_resp_header("location", jay_user_path(conn, :show, user))
       |> render("show.json", user: user)
     end
   end
--- test/afy_web/controllers/v1/jay_user_controller_test.exs
+++ test/afy_web/controllers/v1/jay_user_controller_test.exs
@@ -19,17 +19,17 @@ defmodule AfyWeb.V1.JayUserControllerTest do
 
   describe "index" do
     test "lists all users", %{conn: conn} do
-      conn = get conn, user_path(conn, :index)
+      conn = get conn, jay_user_path(conn, :index)
       assert json_response(conn, 200)["data"] == []
     end
   end
 
   describe "create user" do
     test "renders user when data is valid", %{conn: conn} do
-      conn = post conn, user_path(conn, :create), user: @create_attrs
+      conn = post conn, jay_user_path(conn, :create), user: @create_attrs
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
-      conn = get conn, user_path(conn, :show, id)
+      conn = get conn, jay_user_path(conn, :show, id)
       assert json_response(conn, 200)["data"] == %{
         "id" => id,
         "device_token" => "some device_token",
@@ -37,7 +37,7 @@ defmodule AfyWeb.V1.JayUserControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post conn, user_path(conn, :create), user: @invalid_attrs
+      conn = post conn, jay_user_path(conn, :create), user: @invalid_attrs
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -46,10 +46,10 @@ defmodule AfyWeb.V1.JayUserControllerTest do
     setup [:create_user]
 
     test "renders user when data is valid", %{conn: conn, user: %User{id: id} = user} do
-      conn = put conn, user_path(conn, :update, user), user: @update_attrs
+      conn = put conn, jay_user_path(conn, :update, user), user: @update_attrs
       assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
-      conn = get conn, user_path(conn, :show, id)
+      conn = get conn, jay_user_path(conn, :show, id)
       assert json_response(conn, 200)["data"] == %{
         "id" => id,
         "device_token" => "some updated device_token",
@@ -57,7 +57,7 @@ defmodule AfyWeb.V1.JayUserControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn, user: user} do
-      conn = put conn, user_path(conn, :update, user), user: @invalid_attrs
+      conn = put conn, jay_user_path(conn, :update, user), user: @invalid_attrs
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -66,10 +66,10 @@ defmodule AfyWeb.V1.JayUserControllerTest do
     setup [:create_user]
 
     test "deletes chosen user", %{conn: conn, user: user} do
-      conn = delete conn, user_path(conn, :delete, user)
+      conn = delete conn, jay_user_path(conn, :delete, user)
       assert response(conn, 204)
       assert_error_sent 404, fn ->
-        get conn, user_path(conn, :show, user)
+        get conn, jay_user_path(conn, :show, user)
       end
     end
   end
